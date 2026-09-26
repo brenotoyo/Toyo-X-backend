621f2597eb72ca0e1362d4c3dcb12f7ea615d747
Breno Kazuo <breno_toyo@hotmail.com>
2026-09-26 12:12:10 -0300

    inicializando cloudinary no settings

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,13 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
 }
 
+import cloudinary
+cloudinary.config(
+    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    api_key    = os.environ.get('CLOUDINARY_API_KEY', ''),
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET', ''),
+)
+
 if os.environ.get('CLOUDINARY_CLOUD_NAME'):
     # Produção: usa Cloudinary para mídia e Whitenoise para estáticos
     STORAGES = {
